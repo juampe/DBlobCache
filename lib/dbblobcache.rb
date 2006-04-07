859c1eb308d2faf051878f3e7a5ced34c820ed04
www-data <www-data@f63d5e45-9e10-0410-995a-edf4afa21b8d>
2006-04-07 19:20:27 +0000

    fix bugs and make usable again
    git-svn-id: file:///var/lib/svn/dbblobcache/trunk@10 f63d5e45-9e10-0410-995a-edf4afa21b8d

--- lib/dbblobcache.rb
+++ lib/dbblobcache.rb
@@ -6,10 +6,10 @@ require "dbi"
 require 'drb'
 
 class DBBlobCache
-  def initialize(tags)
-    #Script must be run at the rails root oath
+  def initialize
+    puts "initialize"
+    #Script must be run at the rails root path
     @rails_root=__FILE__.sub!(/vendor\/plugins\/dbblobcache\/.*/,'')
-    #@base=__FILE__.sub!(/lib\/.*/,'')
     Dir.chdir(@rails_root)
     #The yalm enviroment in database.yml
     env="dbblobcache"
@@ -28,8 +28,17 @@ class DBBlobCache
         # disconnect from server
       @dbh.disconnect if @dbh
     end
-    @@tags=tags
-    @@tags={:thumb => "image_scale:150x150+image_crop:100x100",:zip =>"compress_zip:3",:utf8 => "text_encode:utf8"}
+    #Set default tags
+    @tags={
+      :thumb => "image_scale:150x150",
+      :medium => "image_scale:500x500",
+      :zip =>"compress_zip:3",
+      :utf8 => "text_encode:utf8"
+    }
+  end
+  
+  def set_tags(tags)
+    @tags=tags 
   end
 	  
   def destructor
@@ -42,91 +51,92 @@ class DBBlobCache
   #extension ins the extension of the image due to mime
   #date is the date of last change in database
   def get_image(id,tag,extension,date)
-    (backend)=tag.split(':')
-    if @@tags.has_key(backend)?
-      return cache_file(id,tag,extension,date)
-    else
-      return /public/images/rails.png
-    end
+    return get_file(id,tag,extension,date)
   end
   
   def get_file(id,tag,extension,date)
-    for tag in @@tags[tag].split('+')
+    cached_file=cache_file(id,tag,extension,date)
+    if tag==""
+      puts "get_file "+cached_file[:filename].sub(/^public/,'')
+      return cached_file[:filename].sub(/^public/,'')
+    end
+    for tag in @tags[tag].split('+')
       method=tag.split(':')
       puts method[0]+"->"+method[1]
-      cached_file=cache_file(id,tag,extension,date)
-      cached_file_path=@rails_root+@store_root+@store_directory+cached_file
-      if method[0]==""
-        return cached_file
-      elsif method[0]=="image_scale"
-        if File.stat(cached_file_path).mtime<date
-	end
-        puts "scaling to "+method[1]
-      elsif method[0]=="image_crop"
-        puts "cropping to "+method[1]
-      elsif method[0]=="zip"
-        puts "compress level to "+method[1]
-      else
-        return cached_file
+      begin
+        create=(not File.exists?(cached_file[:filename])) or (File.stat(cached_file[:filename]).mtime<date)
+      rescue
+      end
+      if create
+        if method[0]=="image_scale"
+          if File.stat(cached_file[:tagged_filename]).mtime<date
+            puts "scaling to "+method[1]
+          end
+        elsif method[0]=="image_crop"
+          puts "cropping to "+method[1]
+        elsif method[0]=="zip"
+          puts "compress level to "+method[1]
+        else
+          return cached_file[:tagged_filename].sub(/^public/,'')
+        end
       end
     end
-end
-
-  
-    return cache_file(id,tag,extension,date)
   end
+
   
   #id to databse table definede en database.yml
   #tag is a label to
   def cache_file(id,tag,extension,date)
-    filename=@store_root+@store_directory+build_hash_path(id.to_s+"."+extension)+id.to_s+tag+"."+extension
+    hash=build_hash_path(id.to_s+"."+extension)
+    filename=@store_root+@store_directory+hash+id.to_s+"."+extension
+    tagged_filename=@store_root+@store_directory+hash+id.to_s+tag+"."+extension
     begin
       #The cache logic
       retrieve=(not File.exists?(filename)) or (File.stat(fillename).mtime<date)
     rescue
     end
     if retrieve
-        query="select "	+@store_blob+" from "+
-	    @store_table+" where id='"+id.to_s+"'"
-	begin
-	  sth=@@dbh.prepare(query)
-	  sth.execute
-	  row=sth.fetch
-	  blob=row.by_field(@store_blob)
-	rescue DBI::DatabaseError => e
-	  puts "Error #{e.err} #{e.errstr}"
-	end
+      query="select "	+@store_blob+" from "+
+         @store_table+" where id='"+id.to_s+"'"
+      begin
+        sth=@@dbh.prepare(query)
+        sth.execute
+        row=sth.fetch
+        blob=row.by_field(@store_blob)
+      rescue DBI::DatabaseError => e
+         puts "Error #{e.err} #{e.errstr}"
+      end
 	#puts "caching "+blob.size.to_s
-	begin
-    	  file=File.new(filename,"w")
-	  file.write(blob)
-	  file.close
-	rescue
-	end
+      begin
+        file=File.new(filename,"w")
+        file.write(blob)
+        file.close
+      rescue
+      end
     end
     filename.sub!(/^public/,'')
-    return filename
+    return {:filename => filename , :tagged_filename => tagged_filename}
   end
   
   def delete_file(id,extension)
     filepath=@store_root+@store_directory+build_hash_path(id.to_s+"."+extension)
-    Dir.foreach(filepath){
-      |file|
-      if file.match(#{id.to_s+tag+"."+extension})
-    File.delete(filename)
+    for file in Dir.entries(filepath)
+#      if file.match(#{id.to_s+tag+"."+extension})
+#        File.delete(filename)
+#       end
+    end
   end
   
   private
   def full_mkdir(dir)
     Dir.chdir(@rails_root+@store_root)
-    dir.split('/').each {
-      |mkdir|
+    for mkdir  in dir.split('/')
       begin
         Dir.mkdir(mkdir) if not File.exist?(mkdir)
 	Dir.chdir(mkdir)
       rescue
       end
-    }
+    end
     Dir.chdir(@rails_root)
   end
 
@@ -134,7 +144,6 @@ end
     hash=Digest::MD5.hexdigest(name)
     dir=hash[0,1]+"/"+hash[0,2]
     full_mkdir(@store_directory+dir)
-    puts "build hash "+@store_directory+dir
     return dir+"/"
   end
 end
